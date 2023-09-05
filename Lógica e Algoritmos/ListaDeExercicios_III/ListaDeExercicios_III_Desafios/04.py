24485350799480d6daebc990b4d55c8ea8aea8b0
yrnThiago <thiagodias_123@hotmail.com>
2023-09-05 20:18:01 -0300

    feat: add ex 04

--- Lógica e Algoritmos/ListaDeExercicios_III/ListaDeExercicios_III_Desafios/04.py
+++ Lógica e Algoritmos/ListaDeExercicios_III/ListaDeExercicios_III_Desafios/04.py
@@ -1,1 +1,8 @@
-print()
+def mostra_decomposicao(n: int) -> bool:
+    for divisor in range(2, n):
+        while n % divisor == 0:
+            print(divisor)
+            n /= divisor
+
+n = int(input())
+mostra_decomposicao(n)
